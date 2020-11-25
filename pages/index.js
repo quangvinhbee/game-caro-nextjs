96603b76ab7f4d5019a77d2449dfbd794f06e3b2
quangvinhbee <lamquangvinh686@gmail.com>
2020-11-25 16:24:06 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
+
+import Container from 'react-bootstrap/Container'
 import Layout from '../components/includes/Layout'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
 
 export default function Home() {
   var show = true
+  const arrr = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ].map((variant, idx) => (
+    <Alert key={idx} variant={variant}>
+      This is a {variant} alert—check it out!
+    </Alert>
+  ));
   return (
     <>
+      {arrr}
       <Layout title="Xin chào!"></Layout>
-      Trang chu
+      <Container fluid="md">
+        <Row>
+          <Col>Main 1</Col>
+          <Col xs={4}>Main 1</Col>
+        </Row>
+      </Container>
     </>
   )
 }
