ed34d68fe758afa26c0089b1a9250981e8669e51
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2020-12-05 09:56:55 +0700

    delete [id]

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Layout from '../components/layout/Layout'
 import Layout_Menu from '../components/layout/Layout_Menu'
 
-export default function  Home(props) {
-  Layout_Menu
+export default function Home(props) {
   return (
     <>
       <Layout title="Xin chào!"></Layout>
       <Layout_Menu></Layout_Menu>
-     </>
+    </>
   )
 }
