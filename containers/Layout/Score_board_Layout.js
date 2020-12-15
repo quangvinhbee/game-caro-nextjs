0bcec15009e7d5ce06553437937d2e627e5a8c01
quangvinhbee <72595494+quangvinhbee@users.noreply.github.com>
2020-12-15 23:09:19 +0700

    Update Score_board_Layout.js

--- containers/Layout/Score_board_Layout.js
+++ containers/Layout/Score_board_Layout.js
@@ -6,7 +6,7 @@ const Score_board_Layout = (props) => {
     const { status } = props;
     return (
         <>
-            <Score_board status={status}></Score_board>
+            <Score_board status={status}></Score_board><Score_board status={status}></Score_board>
         </>
     );
 }
